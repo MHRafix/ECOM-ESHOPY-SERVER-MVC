8e57fde2d084592c80f484cf298ecdac9b2209c8
MHRafix <fmhrafiz27@gmail.com>
2022-03-06 14:28:31 +0600

    code push

--- controllers/withoutMVC.js
+++
@@ -1,6 +0,0 @@
-// Save the details of product to the database
-app.post("/addToWishList", async (req, res) => {
-  const favProduct = req.body;
-  const result = await wishListProductsCollection.insertOne(favProduct);
-  res.json(result);
-});
