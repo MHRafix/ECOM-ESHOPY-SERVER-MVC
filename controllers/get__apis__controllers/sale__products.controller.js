a278b6356a92c1f3f184782a6d7f2f4205c0b8fa
MHRafix <fmhrafiz27@gmail.com>
2022-03-13 21:08:10 +0600

    code push

--- controllers/get__apis__controllers/sale__products.controller.js
+++ controllers/get__apis__controllers/sale__products.controller.js
@@ -4,7 +4,10 @@ const products__collection = require("../../models/products.model");
 // get products api controller here
 exports.get__sale__products = async (req, res, next) => {
   try {
-    const sale__products = await products__collection.find();
+    const all__products = await products__collection.find();
+    const sale__products = all__products.filter(
+      (product) => product.salePrice === "0"
+    );
     res.send(sale__products);
   } catch (err) {
     next(err);
